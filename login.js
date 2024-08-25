2a7653482ff3ab5381b194b6b1aaf63c0bc4183e
ines vaz <151469973+1nestable@users.noreply.github.com>
2024-08-25 14:32:47 -0300

    Update login.js

--- login.js
+++ login.js
@@ -6,10 +6,10 @@ document.addEventListener('DOMContentLoaded', function() { //espera a que el DOM
         const username = document.getElementById('username').value.trim(); //obtiene el valor del campo de usuario
         const password = document.getElementById('password').value.trim(); //obtiene el valor del campo de contraseña
 
-        if (username === '' || password === '') { // Si el usuario o la contraseña están vacíos
+        if (username === '' || password === '') { //si el usuario o la contraseña están vacios
             alert('Por favor, complete todos los campos.'); //muestra un mensaje de alerta
         } else {
-            localStorage.setItem('logged', 'true'); // Guarda el estado de autenticación en localStorage
+            localStorage.setItem('logged', 'true'); //guarda el estado de autenticación en localStorage
             
             window.location.href = 'principal.html'; //redirige a la página principal
         }

86abefde12120693dbcbcc7207092099729688a8
ines vaz <151469973+1nestable@users.noreply.github.com>
2024-08-25 14:16:09 -0300

    Update guardadologin.js

--- guardadologin.js
+++ guardadologin.js
@@ -1,30 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const estaLogged = localStorage.getItem('logged'); //autenticación
+    const estaLogged = localStorage.getItem('logged'); // Obtener el estado de autenticación
 
-    //si no esta autenticado, muestra el login
-    if (!estaLogged) {
-        //guarda la URL de la pagina actual antes de redirigir
-        localStorage.setItem('PaginaPrevia', window.location.href);
-
-        //redirige a login.html
-        window.location.href = 'login.html';
-    }
-    if (botonLogin) { // Verifica que el botón de login exista (es decir, que estamos en login.html)
-        botonLogin.addEventListener('click', function() {
-            const user = document.getElementById('username').value;
-            const pass = document.getElementById('password').value;
-
-            //verifica que ambos campos no estén vacíos
-            if (user && pass) {
-                // Simulación de autenticación exitosa
-                localStorage.setItem('logged', 'true'); //guarda el estado de autenticación
-
-                //redirige a la pagina anterior, la pqgina principal
-                const PaginaPrevia = localStorage.getItem('PaginaPrevia') || 'principal.html';
-                window.location.href = PaginaPrevia;
-            } else {
-                alert('Falta completar el campo usuario o contraseña');
-            }
-        });
+    if (estaLogged === 'true') {
+        // Si el usuario ya está autenticado, redirigir a principal.html
+        window.location.href = 'principal.html';
     }
 });

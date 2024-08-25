c0bc652ce6da3cd75c17fb24b1a75f5a1f5b7b8d
ines vaz <151469973+1nestable@users.noreply.github.com>
2024-08-25 13:23:38 -0300

    autenticacion y guardado login.js

--- login.js
+++ login.js
@@ -1,16 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() { //espera a que el DOM esté completamente cargado    
-    const form = document.getElementById('login'); //aelecciona el formulario de login
-    
-    form.addEventListener('submit', function(event) { //agrega un evento al formulario
-        event.preventDefault(); //para prevenir que el formulario se envíe antes de validarlo
-        const username = document.getElementById('username').value.trim(); //obtiene el valor del campo de usuario
-        const password = document.getElementById('password').value.trim(); //obtiene el valor del campo de contraseña
+document.addEventListener('DOMContentLoaded', function() {
+    // Verificar si el usuario ya ha iniciado sesión
+    const estaLogged = localStorage.getItem('logged');
+
+    if (estaLogged) {
+        // Si el usuario ya está autenticado, redirigir a principal.html
+        window.location.href = 'principal.html';
+        return; // Termina la ejecución del script si ya está autenticado
+    }
+
+    // Si el usuario no ha iniciado sesión, configurar el formulario de login
+    const form = document.getElementById('login-form'); // Selecciona el formulario de login
+
+    form.addEventListener('submit', function(event) { 
+        event.preventDefault(); // Prevenir que el formulario se envíe antes de validarlo
+        const username = document.getElementById('username').value.trim(); // Obtiene el valor del campo de usuario
+        const password = document.getElementById('password').value.trim(); // Obtiene el valor del campo de contraseña
 
         if (username === '' || password === '') { // Si el usuario o la contraseña están vacíos
-            alert('Por favor, complete todos los campos.'); //muestra un mensaje de alerta
+            alert('Por favor, complete todos los campos.'); // Muestra un mensaje de alerta
         } else {
+            // Simulación de autenticación exitosa
+            localStorage.setItem('logged', 'true'); // Guarda el estado de autenticación
 
-            window.location.href = 'principal.html'; //redirige a la página principal
+            // Redirigir a la página principal
+            window.location.href = 'principal.html'; 
         }
     });
 });
